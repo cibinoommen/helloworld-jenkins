c72ec02e5f2a6dec95bb76bb104c84ec88034670
cibinoommen <cibin.p.oommen@gmail.com>
2019-06-15 16:19:36 +1000

    feature commit21

--- src/test/java/helloworld/jenkins/LibraryTest.java
+++ src/test/java/helloworld/jenkins/LibraryTest.java
@@ -10,6 +10,6 @@ import org.junit.Test;
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return cibin 'true'", classUnderTest.someLibraryMethod());
+        assertTrue("someLibraryMethod should return cibin 'true'", classUnderTest.someLibraryMethod())
     }
 }
